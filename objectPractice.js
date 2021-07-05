2bec63184267f53a30bd0c939b177521019d219f
keithdrucker <keithdrucker@hotmail.com>
2021-07-05 14:29:38 -0400

    add files

---
+++ objectPractice.js
@@ -0,0 +1,17 @@
+const person = {
+    firstName: "Keith",
+    lastName: "Drucker",
+    age: 73
+}
+
+person.age = 26
+
+var jawn = "age"
+
+
+
+
+
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person[jawn])
