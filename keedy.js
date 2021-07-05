0bef0d633b085bdc71e7b9b2b92a0ed015db8d9d
keithdrucker <keithdrucker@hotmail.com>
2021-07-05 14:31:42 -0400

    easier to read

--- keedy.js
+++ keedy.js
@@ -1,70 +1,75 @@
-// console.log("yeet")
-// let milk = 10
-// const dad = "KAD"
 
-// console.log(milk + " " + dad + "s " + "walk into evvys crib")
-
-// for(let i=0; i<= 10; i++){
-//     if(i % 2 === 0) {
-//         console.log(i)
-//     } 
-// }
 
 // question1
-// function sum(num1, num2) {
-//     return num1 + num2
-// }
+function sum(num1, num2) {
+    return num1 + num2
+}
 
-// console.log(sum(4, 6))
+console.log(sum(4, 6))
+
+
+
 
 // question2
-// function minToSec(min){
-//     return min * 60
-// }
+function minToSec(min){
+    return min * 60
+}
 
-// console.log(minToSec(3))
+console.log(minToSec(3))
+
+
+
 
 // question3
-// function triArea(base, height){
-//     return (base * height) / 2
-// }
+function triArea(base, height){
+    return (base * height) / 2
+}
 
-// console.log(triArea(7, 4))
+console.log(triArea(7, 4))
+
+
+
+
 
 // question4
-// function points(twoPoints, threePoints) {
-//     return (twoPoints * 2) + (threePoints * 3)
-// }
+function points(twoPoints, threePoints) {
+    return (twoPoints * 2) + (threePoints * 3)
+}
 
-// console.log(points(38, 8))
+console.log(points(38, 8))
+
+
+
 
 // question5
+function addUp(num1) {
+    let sum = 0
+    for(let i=1; i<=num1; i++){
+        sum += i
+    }
+    return sum
+}
 
-// function addUp(num1) {
-//     let sum = 0
-//     for(let i=1; i<=num1; i++){
-//         sum += i
-//     }
-//     return sum
-// }
+console.log(addUp(600))
 
-// console.log(addUp(600))
+
 
 // question6
-// function timeForMilkAndCookies(keithsDate) {
-//     if(keithsDate.getMonth() === 11 && keithsDate.getDate() === 24) {
-//         return true
-//     } else {
-//         return false
-//     }
-// }
+function timeForMilkAndCookies(keithsDate) {
+    if(keithsDate.getMonth() === 11 && keithsDate.getDate() === 24) {
+        return true
+    } else {
+        return false
+    }
+}
 
-// const keithsDay = (new Date(1994, 09, 19))
-// console.log(timeForMilkAndCookies(new Date(1994, 11, 24)))
+const keithsDay = (new Date(1994, 09, 19))
+console.log(timeForMilkAndCookies(new Date(1994, 11, 24)))
+
+
 
 
 //question7
-
 function detectWord(crowd){
     const chars = crowd.split("")
 
@@ -84,5 +89,6 @@ function detectWord(crowd){
         }   
         return false;        
     }
+
 console.log(detectWord("UcUNFYGaFYFYGtNUH"))
 checkIsLowerCase("C")
